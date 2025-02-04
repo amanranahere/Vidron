e297723cda81df2b53182971a9e997f4156788cd
Aman Rana <amanranahere@gmail.com>
2025-02-04 21:38:17 +0530

    fix backend/vercel.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,20 +8,8 @@ app.use(
   cors({
     origin: ["https://vidron.vercel.app", "http://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.use((err, req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://vidron.vercel.app");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  res.status(err.status || 500).json({
-    success: false,
-    message: err.message || "Internal Server Error",
-  });
-});
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
