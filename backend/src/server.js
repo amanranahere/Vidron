a327ec439a122ae840d96f865bd74f52b077e5c0
Aman Rana <amanranahere@gmail.com>
2025-02-12 18:53:31 +0530

    add server.js file for running server locally

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/db_index.js";
+
+dotenv.config({ path: "./.env" });
+
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed!", err);
+  });
