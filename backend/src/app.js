3c7be9b4e8bcf08ea07441a92f29f4185d699765
Aman Rana <amanranahere@gmail.com>
2025-02-12 19:00:47 +0530

    test backend server

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Hello from Vidron backend");
+});
+
 // routes import
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
